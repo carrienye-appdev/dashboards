d985d4f39fad3237da65655321c64b8971d38ee2
carrie nye <cnye@chicagobooth.edu>
2023-04-16 23:29:58 +0000

    currency done

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -6,7 +6,7 @@ class CurrenciesController < ApplicationController
 
     @array_of_symbols = @symbols_hash.keys
 
-    render({ :template => "currency_templates/step_one.html.erb"})
+    render({ :template => "currency_templates/step_one.html.erb" })
   end
 
   def second_currency
@@ -18,6 +18,16 @@ class CurrenciesController < ApplicationController
 
     @from_symbol = params.fetch("from_currency")
 
-    render({:template => "currency_templates/step_two.html.erb"})
+    render({ :template => "currency_templates/step_two.html.erb" })
+  end
+
+  def third_currency
+    @from_symbol = params.fetch("from_currency")
+    @conversion_symbol = params.fetch("conversion")
+    @raw_data = open("https://api.exchangerate.host/convert?from=#{@from_symbol}&to=#{@conversion_symbol}").read
+    @parsed_data = JSON.parse(@raw_data)
+    @conversion_hash = @parsed_data.fetch("result")
+
+    render({ :template => "currency_templates/step_three.html.erb" })
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get("/forex", { :controller => "currencies", :action => "first_currency"})
 
   get("/forex/:from_currency", { :controller => "currencies", :action => "second_currency"})
+
+  get("/forex/:from_currency/:conversion", { :controller => "currencies", :action => "third_currency"})
 end
